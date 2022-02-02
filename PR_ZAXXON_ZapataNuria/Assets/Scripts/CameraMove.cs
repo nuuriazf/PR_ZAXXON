50b1720b09af1d7e54e78f9fa63c29b0310b85bd
Celia Rico <nuriellez@gmail.com>
2022-02-02 10:26:32 +0100

    Añadiendo texturas 1

--- PR_ZAXXON_ZapataNuria/Assets/Scripts/CameraMove.cs
+++ PR_ZAXXON_ZapataNuria/Assets/Scripts/CameraMove.cs
@@ -22,7 +22,7 @@ public class CameraMove : MonoBehaviour
         
         // sigue al objeto 
         //La velocidad de suavizado, cuanto menor sea más brusco será el movimiento
-        Vector3 targetPosition = new Vector3(playerPosition.position.x, playerPosition.position.y, transform.position.z);
+        Vector3 targetPosition = new Vector3(playerPosition.position.x, playerPosition.position.y + 1f, transform.position.z);
         transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref camaraVelocity, smoothVelocity);
         
        
